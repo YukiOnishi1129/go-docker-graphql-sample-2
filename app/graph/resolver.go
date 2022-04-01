23195c2f628114c2a23c67940ecec5c6b471a982
yuki <yuki.onishi1129@gmail.com>
2022-04-02 04:10:31 +0900

    todo service層の作成

--- app/graph/resolver.go
+++ app/graph/resolver.go
@@ -1,11 +1,25 @@
 package graph
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/YukiOnishi1129/go-docker-graphql-sample-2/app/service/todo"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	DB *sql.DB
+	db              *sql.DB
+	todoServiceFunc todo.LazyInitFunc
+}
+
+func NewResolver(
+	db *sql.DB,
+	todoServiceFunc todo.LazyInitFunc,
+) *Resolver {
+	return &Resolver{
+		db:              db,
+		todoServiceFunc: todoServiceFunc,
+	}
 }
--- app/service/todo/todo_service.go
+++ app/service/todo/todo_service.go
@@ -1,1 +1,48 @@
 package todo
+
+import (
+	"context"
+	"database/sql"
+	"github.com/YukiOnishi1129/go-docker-graphql-sample-2/app/database/entity"
+	"github.com/YukiOnishi1129/go-docker-graphql-sample-2/app/graph/model"
+	"strconv"
+)
+
+type Service struct {
+	db *sql.DB
+}
+
+type LazyInitFunc func(db *sql.DB) *Service
+
+func LazyInit() LazyInitFunc {
+	return func(db *sql.DB) *Service {
+		return &Service{
+			db: db,
+		}
+	}
+}
+
+const TIME_LAYOUT = "2006-01-02 15:04:05"
+
+func (s *Service) TodoList(ctx context.Context) ([]*model.Todo, error) {
+	todoList, todoErr := entity.Todos().All(ctx, s.db)
+	if todoErr != nil {
+		return nil, todoErr
+	}
+	resTodoList := make([]*model.Todo, len(todoList))
+	for i, todo := range todoList {
+		resTodo := model.Todo{
+			ID:        strconv.FormatUint(todo.ID, 10),
+			Text:      todo.Title,
+			Comment:   todo.Comment,
+			CreatedAt: todo.CreatedAt.Format(TIME_LAYOUT),
+			UpdatedAt: todo.UpdatedAt.Format(TIME_LAYOUT),
+		}
+		if todo.DeletedAt.Valid {
+			deletedAt := todo.DeletedAt.Time.Format(TIME_LAYOUT)
+			resTodo.DeletedAt = &deletedAt
+		}
+		resTodoList[i] = &resTodo
+	}
+	return resTodoList, nil
+}
