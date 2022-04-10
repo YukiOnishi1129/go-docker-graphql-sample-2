b7cf46fdd6142e6cf60a8a4c8ffd60500e774f00
yuki <yuki.onishi1129@gmail.com>
2022-04-10 23:47:04 +0900

    シェルスクリプト

--- dev-tools/bin/runner.sh
+++ dev-tools/bin/runner.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
-# シェルスクリプト サンプル
-#ARD=${@:2}
-
 #DB_PASSWORD="pass"
-TEST_DB_ENV_STRING="test"
+#TEST_DB_ENV_STRING="test"
+# コマンド実行時の第2引数以下をARGに格納
+#ARG_SECOND=${@:2}
+#ARG_THIRD=${@:3}
+#ARG_FORTH=${@:4}
 
 case ${1} in
+# テーブルからentityを自動生成 (事前にdbコンテナを立ち上げておくこと)
+"create:entity")
+ sqlboiler mysql -c app/database.toml -o app/database/entity -p entity --no-tests --wipe
+ ;;
+"migrate:up")
+ migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' up
+ ;;
+"migrate:down")
+ migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' down
+ ;;
+"lint")
+ # shellcheck disable=SC2164
+ cd app
+ staticcheck ./...
+ ;;
 "test:all")
-#  cd app
-#  go test -v ./...
-  docker-compose up -d server db-test
-  docker-compose exec -e TEST_DB_ENV="${TEST_DB_ENV_STRING}" server go test -v ./...
-#  go test -v ./...
-#  docker-compose exec -e server go test -v ./...
-  docker-compose stop
-  ;;
+ # shellcheck disable=SC2164
+ cd app
+ go test -v ./...
+ ;;
+#"test")
+# # shellcheck disable=SC2164
+# cd app
+# go test -v "${ARG_SECOND}" "${ARG_THIRD}" "${ARG_FORTH}"
+# ;;
+"gql")
+# shellcheck disable=SC2164
+ cd app
+ gqlgen generate
+ ;;
 esac
