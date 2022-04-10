62b1389ba13425ebab0732a20a85827cd4b2a5aa
yuki <yuki.onishi1129@gmail.com>
2022-04-11 02:18:46 +0900

    db migration修正

--- dev-tools/bin/runner.sh
+++ dev-tools/bin/runner.sh
@@ -9,24 +9,57 @@
 
 case ${1} in
 # テーブルからentityを自動生成 (事前にdbコンテナを立ち上げておくこと)
-"create:entity")
+"entity:create")
+ echo  === entity create start ===
  sqlboiler mysql -c app/database.toml -o app/database/entity -p entity --no-tests --wipe
+ echo  === entity create end ===
  ;;
-"migrate:up")
+"db:migrate")
+ echo  === db migrate start ===
  migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' up
+ echo  === db migrate end ===
  ;;
-"migrate:down")
+"db:seed")
+ echo  === db seed start ===
+ # shellcheck disable=SC2164
+ cd app
+ go run database/seed/seed.go
+ echo  === db seed start ===
+ ;;
+"db:rollback")
+ echo  === db rollback start ===
  migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' down
+ echo  === db rollback end ===
+ ;;
+"db:reset")
+ # db:rollback
+ echo  === db rollback start ===
+ migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' down
+ echo  === db rollback end ===
+ # db:migrate
+ echo  === db migrate start ===
+ migrate -source file://app/database/migrations -database 'mysql://user:pass@tcp(127.0.0.1:3306)/20220328_GO_GRAPHQL_DB' up
+ echo  === db migrate end ===
+ # db:seed
+ echo  === db seed start ===
+ # shellcheck disable=SC2164
+ cd app
+ go run database/seed/seed.go
+ echo  === db seed end ===
  ;;
 "lint")
+ echo  === staticcheck lint start ===
  # shellcheck disable=SC2164
  cd app
  staticcheck ./...
+ echo  === staticcheck lint end ===
  ;;
 "test:all")
+ echo  === test all start ===
  # shellcheck disable=SC2164
  cd app
  go test -v ./...
+ echo  === test all end ===
  ;;
 #"test")
 # # shellcheck disable=SC2164
@@ -34,8 +67,10 @@ case ${1} in
 # go test -v "${ARG_SECOND}" "${ARG_THIRD}" "${ARG_FORTH}"
 # ;;
 "gql")
-# shellcheck disable=SC2164
+ echo  === gqlgen generate start ===
+ # shellcheck disable=SC2164
  cd app
  gqlgen generate
+ echo  === gqlgen generate end ===
  ;;
 esac
